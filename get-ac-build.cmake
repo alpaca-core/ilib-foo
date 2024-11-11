966153fd0b4f1ddb85a9508ea78d2b8decaa3b08
Borislav Stanimirov <b@ibob.bg>
2024-11-11 14:12:25 +0200

    build: adapt to renamed ac-build

--- get-ac-build.cmake
+++ get-ac-build.cmake
@@ -4,17 +4,17 @@
 
 # cpm
 if(NOT CPM_SOURCE_CACHE AND NOT DEFINED ENV{CPM_SOURCE_CACHE})
-    set(CPM_SOURCE_CACHE "${CMAKE_SOURCE_DIR}/.cpm")
+    set(CPM_SOURCE_CACHE "${CMAKE_SOURCE_DIR}/.cpm" CACHE PATH "CPM source cache")
     message(STATUS "Setting cpm cache dir to: ${CPM_SOURCE_CACHE}")
 endif()
 include(./get_cpm.cmake)
 
-# ac-local-cmake-lib
-if(AC_MONO_ROOT OR AC_MONO_COMP)
-    set(acLocalCMakeLibDir "${CMAKE_SOURCE_DIR}/../ac-local-cmake-lib")
+# ac-build
+if(AC_BUILD_MONO)
+    set(acLocalCMakeLibDir "${CMAKE_SOURCE_DIR}/../ac-build")
 else()
-    CPMAddPackage(gh:alpaca-core/ac-local-cmake-lib@0.1.0)
-    set(acLocalCMakeLibDir "${ac-local-cmake-lib_SOURCE_DIR}")
+    CPMAddPackage(gh:alpaca-core/ac-build@1.0.0)
+    set(acLocalCMakeLibDir "${ac-build_SOURCE_DIR}")
 endif()
 
 list(APPEND CMAKE_MODULE_PATH
