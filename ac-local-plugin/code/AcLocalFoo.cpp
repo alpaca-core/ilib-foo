4ac69b15e5b4343708b252761149b74b1db094b1
Borislav Stanimirov <b@ibob.bg>
2024-11-22 11:27:20 +0200

    feat: use schema generation

--- ac-local-plugin/code/AcLocalFoo.cpp
+++ ac-local-plugin/code/AcLocalFoo.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) Alpaca Core
 // SPDX-License-Identifier: MIT
 //
-#include <ac/schema/ModelSchema.hpp>
-
 #include <ac/foo/Model.hpp>
 #include <ac/foo/Instance.hpp>
 
@@ -17,55 +15,11 @@
 #include <itlib/throw_ex.hpp>
 #include <stdexcept>
 
+#include "foo-schema.hpp"
 #include "aclp-foo-version.h"
 #include "aclp-foo-interface.hpp"
 
 namespace ac::local {
-
-namespace schema {
-
-struct Foo : public ModelHelper<Foo> {
-    static inline constexpr std::string_view id = "foo";
-    static inline constexpr std::string_view description = "Example inference plugin";
-
-    struct Params : public Object {
-        using Object::Object;
-        String spliceString{*this, "splice_string", "String to splice model data with input"};
-    };
-
-    struct InstanceGeneral : public InstanceHelper<InstanceGeneral> {
-        static inline constexpr std::string_view id = "general";
-        static inline constexpr std::string_view description = "General instance";
-
-        struct Params : public Object {
-            using Object::Object;
-            Int cutoff{*this, "cutoff", "Cut off model data to n-th element (or don't cut if -1)", -1};
-        };
-
-        struct OpRun {
-            static inline constexpr std::string_view id = "run";
-            static inline constexpr std::string_view description = "Run the foo inference and produce some output";
-
-            struct Params : public Object {
-                using Object::Object;
-                Array<String> input{*this, "input", "Input items"};
-                Bool splice{*this, "splice", "Splice input with model data (otherwise append model data to input)", true};
-                Int throwOn{*this, "throw_on", "Throw exception on n-th token (or don't throw if -1)", -1};
-            };
-
-            struct Return : public Object {
-                using Object::Object;
-                String result{*this, "result", "Output text (tokens joined with space)", {}, true};
-            };
-        };
-
-        using Ops = std::tuple<OpRun>;
-    };
-
-    using Instances = std::tuple<InstanceGeneral>;
-};
-
-} // namespace schema
 
 using throw_ex = itlib::throw_ex<std::runtime_error>;
 
@@ -76,9 +30,9 @@ public:
     using Schema = ac::local::schema::Foo::InstanceGeneral;
 
     static foo::Instance::InitParams InitParams_fromDict(Dict& d) {
-        Schema::Params schemaParams(d);
+        auto schemaParams = Schema::Params::fromDict(d);
         foo::Instance::InitParams ret;
-        ret.cutoff = schemaParams.cutoff.getValue();
+        ret.cutoff = schemaParams.cutoff;
         return ret;
     }
 
@@ -88,39 +42,25 @@ public:
     {}
 
     Dict run(Dict& params) {
-        const Schema::OpRun::Params schemaParams(params);
-
-        if (!schemaParams.input.hasValue()) {
-            throw_ex{} << "Missing input";
-        }
-
-        std::vector<std::string> input;
-        input.reserve(schemaParams.input.size());
-        for (size_t i = 0; i < schemaParams.input.size(); ++i) {
-            input.push_back(std::string(schemaParams.input[i].getValue()));
-        }
+        const auto schemaParams = Schema::OpRun::Params::fromDict(params);
 
         foo::Instance::SessionParams sparams;
-        sparams.splice = schemaParams.splice.getValue();
-        sparams.throwOn = schemaParams.throwOn.getValue();
+        sparams.splice = schemaParams.splice;
+        sparams.throwOn = schemaParams.throwOn;
 
-        auto s = m_instance.newSession(std::move(input), sparams);
+        auto s = m_instance.newSession(std::move(schemaParams.input), sparams);
 
-        Dict ret;
-        Schema::OpRun::Return schemaRet(ret);
-
-        std::string result;
+        Schema::OpRun::Return ret;
         for (auto& w : s) {
-            result += w;
-            result += ' ';
+            ret.result += w;
+            ret.result += ' ';
         }
-        if (!result.empty()) {
+        if (!ret.result.empty()) {
             // remove last space
-            result.pop_back();
+            ret.result.pop_back();
         }
 
-        schemaRet.result.setValue(std::move(result));
-        return ret;
+        return ret.toDict();
     }
 
     virtual Dict runOp(std::string_view op, Dict params, ProgressCb) override {
@@ -140,9 +80,9 @@ public:
     using Schema = ac::local::schema::Foo;
 
     static foo::Model::Params ModelParams_fromDict(Dict& d) {
-        Schema::Params schemaParams(d);
+        auto schemaParams = Schema::Params::fromDict(d);
         foo::Model::Params ret;
-        ret.splice = std::string(schemaParams.spliceString.optGetValue().value_or(""));
+        ret.splice = astl::move(schemaParams.spliceString.value_or(""));
         return ret;
     }
 
@@ -171,6 +111,10 @@ public:
             .inferenceSchemaTypes = {"foo"},
         };
         return i;
+    }
+
+    virtual bool canLoadModel(const ModelDesc&, const Dict&) const noexcept override {
+        return true;
     }
 
     virtual ModelPtr loadModel(ModelDesc desc, Dict params, ProgressCb pcb) override {
